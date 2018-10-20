cd7895d39041c4937e8c5c6a810d38b4e3c6e731
sanjeev <sanjeevh.93@gmail.com>
2018-10-21 00:18:01 +0530

    Added docker file and runscript

---
+++ run-corda.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# If variable not present use default values
+: ${CORDA_HOME:=/opt/corda/src/build/nodes/${NODE_NAME}}
+: ${JAVA_OPTIONS:=-Xmx512m}
+
+export CORDA_HOME JAVA_OPTIONS
+
+cd ${CORDA_HOME}
+#java $JAVA_OPTIONS -jar ${CORDA_HOME}/corda-webserver.jar 2>&1 &
+#java $JAVA_OPTIONS -jar ${CORDA_HOME}/corda.jar 2>&1
+nohup java $JAVA_OPTIONS -jar corda.jar & java $JAVA_OPTIONS -jar corda-webserver.jar
